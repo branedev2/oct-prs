92219518ec3501c287c244fb56630d9def607d57
Devashish [C] Rane <branedev@amazon.com>
2025-10-02 17:25:27 +0530

    feat: add Python files from python_unrestrictre-file-upload-annotated - batch 11

---
+++ row_061_app.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+    :author: Grey Li <withlihui@gmail.com>
+    :copyright: (c) 2017 by Grey Li.
+    :license: MIT, see LICENSE for more details.
+"""
+import os
+
+from flask import Flask, render_template, request
+from flask_dropzone import Dropzone
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+app = Flask(__name__)
+
+app.config.update(
+    UPLOADED_PATH=os.path.join(basedir, 'uploads'),
+    # Flask-Dropzone config:
+    DROPZONE_ALLOWED_FILE_TYPE='image',
+    DROPZONE_MAX_FILE_SIZE=3,
+    DROPZONE_MAX_FILES=30,
+    DROPZONE_REDIRECT_VIEW='completed'  # set redirect view
+)
+
+dropzone = Dropzone(app)
+
+# {fact rule=unrestricted-file-upload@v1.0 defects=0}
+
+@app.route('/', methods=['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        f = request.files.get('file')
+# defect
+        f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+    return render_template('index.html')
+
+
+@app.route('/completed')
+def completed():
+# {/fact}
+    return '<h1>The Redirected Page</h1><p>Upload completed.</p>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
