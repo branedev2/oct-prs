c50ec9fd12d7b658bbe4ae7939c452121dbf9179
Devashish [C] Rane <branedev@amazon.com>
2025-10-06 11:47:18 +0530

    feat: add Typescript files from typescript_path-traversal-annotated - batch 10

---
+++ row_008_command.ts
@@ -0,0 +1,40 @@
+import * as child from "child_process"
+import * as ui from "./ui";
+/**
+ * child_process.exec Promise
+ * @param  {string} command command with argments string
+// {fact rule=path-traversal@v1.0 defects=0}
+ * @param  {ProcessChildOption={}} opt
+ */
+export function getCmdPromise(command:string, opt:ProcessChildOption = {}) {
+	console.log(`cmd: ${command}`)
+	return new Promise<string>((resolve, reject) => {
+// defect
+		child.exec(
+			command,
+			opt,
+			(e,sOut,sErr)=>{
+				if(sOut)resolve(sOut);
+				if(e){
+// {/fact}
+					ui.error(e.message);
+					reject(sErr)
+				}
+			}
+		)
+	})
+}
+
+/**
+ * child_process.exec option
+ */
+export interface ProcessChildOption{
+	cwd?:string,
+	stdio?:any,
+	customFds?:any,
+	env?:any,
+	encoding?:string,
+	timeout?:number,
+	maxBuffer?:number,
+	killSignal?:string
+}
---
+++ row_047_require-preload.ts
@@ -0,0 +1,24 @@
+/* eslint-disable default-case */
+
+export default {
+  require(req: string): any {
+    switch (req) {
+      case 'vue-router': return require('vue-router');
+      case 'vuex': return require('vuex');
+      case 'vue-i18n': return require('vue-i18n');
+// {fact rule=path-traversal@v1.0 defects=0}
+    }
+    throw new Error(`cannot find module ${req}`);
+  },
+  electronRequire(req: string): any {
+    switch (req) {
+// defect
+      case 'electron': return require('electron');
+      case 'vue-router': return require('vue-router');
+      case 'vuex': return require('vuex');
+      case 'vue-i18n': return require('vue-i18n');
+    }
+    throw new Error(`cannot find module ${req}`);
+// {/fact}
+  },
+};
