a9b5c496ef0e3419d85915ea99dd419cc2cca250
Devashish [C] Rane <branedev@amazon.com>
2025-10-06 11:15:39 +0530

    feat: add Typescript files from typescript_code-injection-annotated - batch 08

---
+++ row_001_README.ts
@@ -0,0 +1,38 @@
+import Bluebird from "bluebird";
+// tslint:disable-next-line:no-implicit-dependencies
+import Handlebars from "handlebars";
+import request from "request";
+
+// {fact rule=code-injection@v1.0 defects=1}
+import { readFile, writeFile } from "./lib/utility/fs";
+
+import { execSync } from "child_process";
+
+Handlebars.registerHelper("bash", (command: string) => {
+// defect
+  return execSync(command).toString("ascii");
+});
+
+async function fromGithub(endpoint: string) {
+  return Bluebird.promisify(request)({
+    method: "GET",
+// {/fact}
+    url: "https://api.github.com/" + endpoint,
+    json: true,
+    headers: { "User-Agent": "README generator" }
+  }).then((response: request.RequestResponse & { body: any }) => response.body);
+}
+
+Promise.all([
+  readFile("./README.handlebars", "utf8"),
+  fromGithub("repos/2fd/graphdoc"),
+  fromGithub("repos/2fd/graphdoc/contributors").then(
+    (contributors: Array<{ login: string }>) =>
+      contributors.filter(c => c.login !== "2fd")
+  )
+]).then(([template, project, contributors]) => {
+  return writeFile(
+    "README.md",
+    Handlebars.compile(template)({ project, contributors })
+  );
+});
