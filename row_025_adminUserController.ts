09eb5c7815eaae9915ab00a18a4b7ace44c72a40
Devashish [C] Rane <branedev@amazon.com>
2025-10-06 11:29:24 +0530

    feat: add Typescript files from typescript_os-command-injection-annotated - batch 07

---
+++ row_025_adminUserController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+
+// {fact rule=os-command-injection@v1.0 defects=1}
+export default (dependencies: any) => {
+    const { adminUserUseCase } = dependencies.useCase;
+    const adminUserController = async (req: Request, res: Response) => {
+        try {
+            // Call the executeFunction method of the adminUserUseCase with any required data
+// defect
+            const response = await adminUserUseCase(dependencies).executeFunction(req.body);
+            
+
+            // Check if the response object and its data property are defined
+            if (response && response.status && response.data) {
+                res.json({ status: true, data: response.data });
+// {/fact}
+            } else {
+                res.json({ status: false, message: "Data not found" });
+            }
+        } catch (error) {
+            console.error("Error in adminUserController:", error);
+            res.status(500).json({ status: false, message: "Internal server error" });
+        }
+    };
+
+    return adminUserController;
+};
